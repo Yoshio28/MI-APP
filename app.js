64d41fced265fef77d0cde686fd00974101c005e
Yoshio <136761497+Yoshio28@users.noreply.github.com>
2025-11-03 20:13:28 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const usuarioController = require("./controllers/usuarioController");
 
 app.set("view engine", "ejs");
 
+const PORT = 3000;
 app.get("/", usuarioController.mostrarUsuarios);
 
 app.listen(3000, '0.0.0.0', () => {
